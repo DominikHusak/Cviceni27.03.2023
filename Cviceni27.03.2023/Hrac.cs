d3e8fb18919c4c0a89b5bff453e0cd80ed625084
Dominik Husák <domca.husak@gmail.com>
2023-04-02 09:39:03 +0200

    second commit

--- Cviceni27.03.2023/Hrac.cs
+++ Cviceni27.03.2023/Hrac.cs
@@ -8,5 +8,70 @@ namespace Cviceni27._03._2023
 {
     internal class Hrac
     {
+        private readonly object healthLock = new object(); // lock object for accessing health property
+        private readonly object aliveLock = new object(); // lock object for accessing alive property
+
+        public string Jmeno { get; set; }
+        private int health;
+        public int Health
+        {
+            get
+            {
+                lock (healthLock)
+                {
+                    return health;
+                }
+            }
+            set
+            {
+                lock (healthLock)
+                {
+                    if (Alive)
+                    {
+                        health = value;
+                    }
+                }
+            }
+        }
+        private bool alive;
+        public bool Alive
+        {
+            get
+            {
+                lock (aliveLock)
+                {
+                    return alive;
+                }
+            }
+            set
+            {
+                lock (aliveLock)
+                {
+                    alive = value;
+                }
+            }
+        }
+
+        public Hrac(string jmeno, int health)
+        {
+            Jmeno = jmeno;
+            Health = health;
+            Alive = true;
+        }
+
+        public void Damage(int amount)
+        {
+            Health -= amount;
+            if (Health <= 0)
+            {
+                Health = 0;
+                Alive = false;
+            }
+        }
+
+        public void Heal(int amount)
+        {
+            Health += amount;
+        }
     }
 }
--- Cviceni27.03.2023/Program.cs
+++ Cviceni27.03.2023/Program.cs
@@ -10,7 +10,45 @@ namespace Cviceni27._03._2023
     {
         static void Main(string[] args)
         {
-                
+            var hrac = new Hrac("John", 100);
+
+            var damageThread = new Thread(() =>
+            {
+                var random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    var damage = random.Next(1, 11);
+                    hrac.Damage(damage);
+                    Console.WriteLine($"{hrac.Jmeno} took {damage} damage. Health: {hrac.Health}");
+                    Thread.Sleep(500);
+                }
+            });
+
+            var healThread = new Thread(() =>
+            {
+                var random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    var heal = random.Next(1, 11);
+                    hrac.Heal(heal);
+                    Console.WriteLine($"{hrac.Jmeno} healed {heal} health. Health: {hrac.Health}");
+                    Thread.Sleep(500);
+                }
+            });
+
+            damageThread.Start();
+            healThread.Start();
+
+            damageThread.Join();
+            healThread.Join();
+
+            if (hrac.Alive)
+            {
+                Console.WriteLine($"{hrac.Jmeno} is alive with {hrac.Health} health.");
+            }
+            else
+            {
+                Console.WriteLine($"{hrac.Jmeno} is dead.");
+            }
         }
-    }
-}
+    } 
